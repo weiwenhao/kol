bf35ef7f88cdb3b8235f0adffb73cb472bfbcbc5
weiwenhao <1101140857@qq.com>
2020-07-23 23:16:21 +0800

    feat: 使用 commander.js

---
+++ app/schedule/crawler_instagram.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = {
+  schedule: {
+    interval: '1s', // 1 分钟间隔
+    type: 'all', // 指定所有的 worker 都需要执行
+  },
+  async task() {
+    console.log('php 是世界上最好的语言');
+  },
+};
--- app/service/instagram/client.js
+++ app/service/instagram/client.js
@@ -1,1 +1,9 @@
 // 提供可用客户端
+
+module.exports = {
+    client() {
+        // 从 redis 读取登录状态
+
+        // 返回 ig client
+    }
+};
