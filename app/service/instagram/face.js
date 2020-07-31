80fea68290453e7ccde19064e098f179dd2ec3a7
weiwenhao <1101140857@qq.com>
2020-07-31 21:38:35 +0800

    feat: face 接口错误处理

--- app/service/instagram/face.js
+++ app/service/instagram/face.js
@@ -11,7 +11,9 @@ class FaceService extends Service {
   constructor(ctx) {
     super(ctx);
 
-    this.pool = [];
+    this.app.logger.info('[instagram-face] 人脸识别程序启动');
+    this.client = new ApiFaceClient(...Object.values(this.app.config.faceApi));
+    this.proxyClient = new HttpsProxyAgent(this.app.config.proxy);
   }
 
   // 不能挂，双进程
@@ -19,9 +21,6 @@ class FaceService extends Service {
     const Op = Sequelize.Op;
     const loop = true;
     const { ctx, app } = this;
-    app.logger.info('[instagram-face] 人脸识别程序启动');
-    const client = new ApiFaceClient(...Object.values(app.config.faceApi));
-    const proxyClient = new HttpsProxyAgent(app.config.proxy);
 
     while (loop) {
       const user = await ctx.model.User.findOne({
@@ -40,34 +39,61 @@ class FaceService extends Service {
       if (user.origin.hd_profile_pic_versions && user.origin.hd_profile_pic_versions.length > 1) {
         url = user.origin.hd_profile_pic_versions[1].url;
       }
-
-      // 图像转 base64
-      const response = await fetch(url, {
-        agent: proxyClient,
-      });
-      const buffer = await response.buffer();
-      const image = buffer.toString('base64');
-
-      // 调用人脸检测
-      const result = await client.detect(image, 'BASE64', {
-        face_field: 'age,beauty,gender,expression,race,emotion',
-      });
-
-      app.logger.info(`[instagram-face] 人脸识别url: ${url}`);
-      if (result.error_code !== 0) {
-        console.log(`[instagram] 识别异常，user_id: ${user.id} error_code: ${result.error_code}, error_msg: ${result.error_msg}`);
-      } else {
-        const face = result.result.face_list[0];
-        user.age = face.age;
-        user.beauty = face.beauty;
-        user.gender = face.gender.type;
-        user.race = face.race.type;
-        app.logger.info(`[instagram-face] 人脸识别成功, user_id: ${user.id}, 性别: ${face.gender.type}, 年龄：${face.age}, 颜值：${face.beauty}, 人种：${face.race.type}`);
+      app.logger.info(`[instagram-face] user_id:${user.id},人脸识别url: ${url}`);
+      let face = null;
+      try {
+        face = await this.face(url);
+      } catch (error) {
+        this.app.logger.warn(`[instagram-face] 识别异常,等待 10 秒后重试, ${error}`);
+        await this.ctx.helper.sleep(10 * 1000);
+        continue;
       }
 
+
       user.facesAt = dayjs().valueOf();
+      if (!face) {
+        await user.save();
+        continue;
+      }
+
+      user.age = face.age;
+      user.beauty = face.beauty;
+      user.gender = face.gender;
+      user.race = face.race;
       await user.save();
     }
+  }
+
+  async face(url) {
+    const { app } = this;
+    // 图像转 base64
+    const response = await fetch(url, {
+      agent: this.proxyClient,
+    });
+
+    const buffer = await response.buffer();
+    const image = buffer.toString('base64');
+
+    // 调用人脸检测
+    const result = await this.client.detect(image, 'BASE64', {
+      face_field: 'age,beauty,gender,expression,race,emotion',
+    });
+
+
+    if (result.error_code !== 0) {
+      app.logger.info(`[instagram] 识别异常， error_code: ${result.error_code}, error_msg: ${result.error_msg}`);
+      return false;
+    }
+
+    const face = result.result.face_list[0];
+    app.logger.info(`[instagram-face] 人脸识别成功, 性别: ${face.gender.type}, 年龄：${face.age}, 颜值：${face.beauty}, 人种：${face.race.type}`);
+
+    return {
+      age: face.age,
+      beauty: face.beauty,
+      gender: face.gender.type,
+      race: face.race.type,
+    };
   }
 
 }
